59636d45868f504e10fb7676ad963b16bb82f7ce
Radi Totev <raditotev@gmail.com>
2015-07-21 14:04:41 +0100

    Added forms and template for signups and updated URLs for static files

--- src/mvp_landing/urls.py
+++ src/mvp_landing/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,3 +13,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL,
+							document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL,
+							document_root=settings.MEDIA_ROOT)
